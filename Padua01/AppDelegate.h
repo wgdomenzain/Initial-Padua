6ff3dff12afe9a1c9cb14215c75c601b8bb7bee7
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-01-20 12:19:42 -0600

    Initial Commit

---
+++ Padua01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Padua01
+//
+//  Created by Walter Gonzalez Domenzain on 20/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
